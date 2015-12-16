34053f3c4ffb2578d4012d63766d0aa389a9609a
mohanaprasad1994 <mohanaprasad1994@yahoo.co.in>
2015-12-17 00:39:01 +0530

    FTP Server

---
+++ FTP server-client/FTP-SERVER/tcpclient.c
@@ -0,0 +1,286 @@
+/* tcpclient.c */
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+#define SIZE 1
+#define NUMELEM 1000
+
+/////// Directory names are not more than 1024 characters
+/////// CWD only one space
+
+/////// files are byte oriented
+/////// Photo not copying properly (size check and pass)
+/////// no fork
+/*
+argv[2]: ipaddress of server
+argv[1]: port no of server
+ */
+
+int
+main(int argc, char *argv[]) {
+
+    int sock, bytes_received;
+    
+    char send_data[1024], recv_data[1024];
+    struct hostent *host;
+    struct sockaddr_in server_addr;
+    if (argc < 3) {
+        printf("Enter PortNo");
+        exit(0);
+    }
+
+    host = gethostbyname(argv[2]);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[1]));
+    server_addr.sin_addr = *((struct in_addr *) host->h_addr);
+    bzero(&(server_addr.sin_zero), 8);
+
+    if (connect(sock, (struct sockaddr *) &server_addr,
+            sizeof (struct sockaddr)) == -1) {
+        perror("Connect");
+        exit(1);
+    }
+    
+    while(1){
+        //printf("\n(ftp) ");
+        gets(send_data);
+        //puts(send_data);
+        if (strcmp(send_data,"quit")==0){
+            char *data="QUIT";
+            send(sock, data, strlen(data), 0);
+            break;
+        }
+        if (send_data[0]=='l' && send_data[1]=='c' && send_data[2]=='d' && send_data[3]==' '){
+            if(chdir(&send_data[4])==-1)
+                printf(" %s directory doesn't exist",&send_data[4]);
+            else{
+                    char buff[1024];
+                    getcwd(buff,1024);
+                    printf("%s\n",buff);
+                }
+        }        
+        if (send_data[0]=='c' && send_data[1]=='d' && send_data[2]==' '){ // for handling cd
+           char temp[1024];
+           strcpy(temp,"CWD ");
+           strcpy(&temp[4],&send_data[3]);
+           send(sock,temp,strlen(temp),0);
+           bytes_received=recv(sock,recv_data,1024,0);
+           recv_data[bytes_received]='\0';
+           if(bytes_received==0 ){
+               printf("Connection failed\n");
+               break;  
+            }
+           else if(strcmp(recv_data,"OK")!=0){
+               printf("server didn't send OK\n");
+               break;
+               }
+           else{
+              bytes_received=recv(sock,recv_data,1024,0);
+              recv_data[bytes_received]='\0';
+              if(strcmp(recv_data,"Illegal CWD Operation")==0)
+                   printf("Illegal CWD Operation\n");
+              else
+                printf(" changed directory to %s\n",recv_data);
+           }
+        } 
+        
+        if(send_data[0]=='p' && send_data[1]=='w' && send_data[2]=='d' ){ // for handling pwd. Prints both CWD of server and client
+            char buff[1024];
+            getcwd(buff,1024);
+            printf(" present workin directory of client is %s\n",buff);
+            send(sock,"PWD",3,0);
+            bytes_received=recv(sock,recv_data,1024,0);
+            recv_data[bytes_received]='\0';
+            if(bytes_received==0 ){
+               printf("Connection failed\n");
+               break;  
+            }
+           else if(strcmp(recv_data,"OK")!=0){
+               printf("server didn't send OK\n");
+               break;
+               }
+           else{
+               bytes_received=recv(sock,recv_data,1024,0);
+               recv_data[bytes_received]='\0';
+               printf("present working directory of server is %s\n",recv_data);
+           }
+        } 
+        
+        if ( send_data[0]=='g' && send_data[1]=='e' && send_data[2]=='t' && send_data[3]==' '){ // Handles get requests
+           char temp[1024];
+           strcpy(temp,"RETR ");
+           strcpy(&temp[5],&send_data[4]);
+           send(sock,temp,strlen(temp),0);
+           bytes_received=recv(sock,recv_data,1024,0);
+           recv_data[bytes_received]='\0';
+           if(bytes_received==0 ){
+               printf("Connection failed\n");
+               break;  
+            }
+           else if(strcmp(recv_data,"File Name OK")!=0){
+               //printf("%s\n",recv_data);
+               printf("server didn't send \"File Name OK\" or file doesn't exist\n");
+               
+            }
+           else{
+               //printf("HEHEHE\n");
+               //bytes_received=recv(sock,recv_data,1024,0);
+               recv_data[bytes_received]='\0';
+               int sz=atoi(recv_data);
+               //printf("size of file = %d bytes\n",sz);
+               int i;
+               int is_path=0;
+               for(i=strlen(send_data)-1;i>=4;i--)                // if /SD1/a.txt if given, this is the code for finding "a.txt"
+                   if(send_data[i]=='/'){
+                       i++;
+                       is_path=1;
+                       break;
+                   }
+               FILE *fil;
+               if(is_path==1)
+                   fil=fopen(&send_data[i],"w");
+               else 
+                   fil=fopen(&send_data[4],"w");
+               int flag=0; // flag to see if end marker has been reached
+               //printf("HEHEHE");
+               
+               while(flag==0){
+                   bytes_received=recv(sock,recv_data,1002,0);
+                   //printf("%d ",bytes_received);
+                   int i;
+                   //if(sz<=10000)
+                       for (i=0;i<bytes_received-1 && flag==0;i++)          // recieve till end of file is reached
+                           if(recv_data[i]=='@' && recv_data[i+1]=='$'){
+                               flag=1;
+                               bytes_received-=2;
+                           }
+                   fwrite(recv_data,SIZE,bytes_received,fil);
+                   sz-=bytes_received;
+                   //printf("%d\n",sz);
+                   
+               }
+               
+               fclose(fil);
+               printf("DONE\n");
+           }    
+        }       
+            
+        if(send_data[0]=='p' && send_data[1]=='u' && send_data[2]=='t' && send_data[3]==' '){
+                    FILE * fil;
+                    if(!(fil=fopen(&send_data[4],"r")))
+                        printf("No such file\n");
+                    else{
+                       char temp[1024];int read;
+                       strcpy(temp,"STOR ");
+                       strcpy(&temp[5],&send_data[4]);
+                       send(sock,temp,strlen(temp),0);
+                       bytes_received=recv(sock,recv_data,1024,0);
+                       recv_data[bytes_received]='\0';
+                       if(bytes_received==0 ){
+                           printf("Connection failed\n");
+                           break;  
+                        }
+                       else if(strcmp(recv_data,"OK to SEND File")!=0){
+                           //printf("%s\n",recv_data);
+                           printf("server didn't send \"OK to SEND File\" or file doesn't exist\n");
+                           
+                        }
+                        else{
+                            
+                            int flag=0;
+                            while(flag==0){
+                                read=fread(send_data,SIZE,NUMELEM,fil);
+                                //printf("%d",read);
+                                fflush(stdout);
+                                if(read<1000){
+                                    send_data[read]='@';
+                                    send_data[read+1]='$';
+                                    flag=1;
+                                    read+=2;
+                                }
+                                send(sock,send_data,read,0);
+                            }
+                            fclose(fil);
+                            bytes_received=recv(sock,recv_data,1024,0);
+                            recv_data[bytes_received]='\0';
+                            if(strcmp(recv_data,"File Received")==0)
+                                printf("File sent\n");
+                            else
+                                printf("Error in sending file\n");
+                        }
+                        
+                        
+                    }
+        }  
+        
+        if (send_data[0]=='l' && send_data[1]=='s' && (send_data[2]=='\0' || send_data[2]==' ')){
+            send(sock,"NLST",4,0);
+            //printf("HELLO");
+            fflush(stdout);
+            bytes_received=recv(sock,recv_data,1024,0);
+            recv_data[bytes_received]='\0';
+            if(bytes_received==0 ){
+               printf("Connection failed\n");
+               break;  
+             }
+            else if(strcmp(recv_data,"OK")!=0){
+               //printf("%s\n",recv_data);
+               printf("server didn't send \"OK\"\n");
+               
+             }
+            else{
+                
+                int flag=0; // flag to see if end marker has been reached
+               //printf("HEHEHE");
+               
+               while(flag==0){
+                   bytes_received=recv(sock,recv_data,1002,0);
+                   send(sock,"OK",3,0);
+                   //printf("%s\n",recv_data);
+                   int i;
+                   //if(sz<=10000)
+                       for (i=0;i<bytes_received-1 && flag==0;i++)
+                           if(recv_data[i]=='@' && recv_data[i+1]=='$'){
+                               flag=1;
+                               bytes_received-=2;
+                           }
+                   if(flag!=1)
+                       printf("%s\n",recv_data);
+                   else
+                       printf("----------\n");
+                   
+                   
+               }
+               
+               
+               
+            }
+            
+        
+        }
+        
+    
+    }
+    /*bytes_received = recv(sock, recv_data, 1024, 0);
+    recv_data[bytes_received] = '\0';
+    printf("\nRecieved data = %s ", recv_data);*/
+
+    close(sock);
+    
+    return 0;
+}
---
+++ FTP server-client/tcpserver_without_fork.c
@@ -0,0 +1,237 @@
+/* tcpserver.c */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include<dirent.h>
+
+#define SIZE 1
+#define NUMELEM 1000
+int
+main(int argc, char *argv[]) {
+    chdir("FTP-SERVER");
+    
+    char root[1024];
+    getcwd(root,1024);
+    puts(root);
+    int root_len=strlen(root);
+    int sock, connected, bytes_received, true = 1;
+    char send_data[1024], recv_data[1024];
+
+    struct sockaddr_in server_addr, client_addr;
+    int sin_size;
+    if (argc < 2) {
+        printf("Enter PortNo");
+        exit(0);
+    }
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[1]));
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(server_addr.sin_zero), 8);
+
+    if (bind(sock, (struct sockaddr *) &server_addr, sizeof (struct sockaddr)) == -1) {
+        perror("Unable to bind");
+        exit(1);
+    }
+
+    if (listen(sock, 5) == -1) {
+        perror("Listen");
+        exit(1);
+    }
+
+    printf("\nTCPServer Waiting for client on port %s\n", argv[1]);
+    fflush(stdout);
+
+
+    while (1) {
+
+        sin_size = sizeof (struct sockaddr_in);
+
+        connected = accept(sock, (struct sockaddr *) &client_addr, &sin_size);
+
+        printf("\n I got a connection from (%s , %d)",
+                inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        fflush(stdout);        
+        chdir(root);
+        while(1){
+        
+            bytes_received = recv(connected, recv_data, 1024, 0);
+            recv_data[bytes_received] = '\0';
+            printf("bytes recieved=%d\n",bytes_received);
+            if(bytes_received<=0){
+                printf("Connection lost\n");
+                fflush(stdout);
+                break;
+            }
+            printf("\nRecieved data = %s ", recv_data);
+            fflush(stdout);
+            if(strcmp(recv_data,"QUIT")==0){
+                printf("recieved QUIT from (%s , %d)\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                fflush(stdout);
+                break;
+             }
+             
+            if(recv_data[0]=='C' && recv_data[1]=='W' && recv_data[2]=='D' && recv_data[3]==' '){
+                send(connected,"OK",2,0);
+                char buff[1024];
+                getcwd(buff,1024);
+                char temp[1024];
+                strcpy(temp,&recv_data[4]);
+                if(recv_data[4]=='~' || recv_data[4]=='/'){
+                    strcpy(temp,root);
+                    temp[strlen(root)]='/';
+                    strcpy(&temp[strlen(root)+1],&recv_data[5]);
+                    printf("%s\n",temp);
+                }
+                if(chdir(temp)==-1)
+                    send(connected,"Illegal CWD Operation",21,0);
+                else{
+                    char buff2[1024];
+                    getcwd(buff2,1024);
+                    printf("%s\n",buff2);
+                    int flag=0;int i;
+                    for(i=0;i<strlen(root) && flag==0;i++)
+                        if(root[i]!=buff2[i]){
+                            flag=1;
+                            chdir(buff);
+                            send(connected,"Illegal CWD Operation",21,0);
+                        }
+                        if (flag==0 && strcmp(buff2,root)==0)
+                            send(connected,"/",1,0);
+                        else if(flag==0){
+                            send(connected,&buff2[strlen(root)],strlen(&buff2[strlen(root)]),0);
+                        }
+                  }
+                }
+                if(recv_data[0]=='P' && recv_data[1]=='W' && recv_data[2]=='D' && recv_data[3]=='\0'){
+                    //printf("I am here");
+                    send(connected,"OK",2,0);
+                    char buff[1024];
+                    getcwd(buff,1024);
+                    if(strlen(buff)==root_len)
+                        send(connected,"/",1,0);
+                    else
+                        send(connected,&buff[root_len],strlen(buff)-root_len,0);
+                }
+                
+                if(recv_data[0]=='R' && recv_data[1]=='E' && recv_data[2]=='T' && recv_data[3]=='R' && recv_data[4]==' '){
+                    printf("RETR\n");
+                    fflush(stdout);
+                    FILE * fil;int read;
+                    if(!(fil=fopen(&recv_data[5],"r")))
+                        send(connected,"FILE DOES'T EXIST",17,0);
+                    else{
+                        send(connected,"File Name OK",12,0);
+                        printf("HEHEHE");
+                        fflush(stdout);
+                        fseek(fil, 0L, SEEK_END);
+                        int sz = ftell(fil);
+                        fseek(fil, 0L, SEEK_SET);
+                        char temp[1024];
+                        snprintf (temp, sizeof(temp), "%d",sz);
+                        //itoa(sz,temp,10);
+                        //send(connected,temp,strlen(temp),0);
+                        int flag=0;
+                        while(flag==0){
+                            read=fread(send_data,SIZE,NUMELEM,fil);
+                            //printf("%d",read);
+                            fflush(stdout);
+                            if(read<1000){
+                                send_data[read]='@';
+                                send_data[read+1]='$';
+                                flag=1;
+                                read+=2;
+                            }
+                            send(connected,send_data,read,0);
+                        }
+                        fclose(fil);
+                    }
+                }
+                
+                if(recv_data[0]=='S' && recv_data[1]=='T' && recv_data[2]=='O' && recv_data[3]=='R' && recv_data[4]==' '){
+                    send(connected,"OK to SEND File",15,0);
+                    FILE *fil;
+                    int i;
+                    int is_path=0;
+                    for(i=strlen(recv_data);i>=5;i--)                // if /SD1/a.txt if given, this is the code for finding "a.txt"
+                        if(recv_data[i]=='/'){
+                           i++;
+                           is_path=1;
+                           break;
+                        }
+                    if(is_path==1)
+                        fil=fopen(&recv_data[i],"w");
+                    else
+                        fil=fopen(&recv_data[5],"w");
+                    int flag=0; // flag to see if end marker has been reached
+                   //printf("HEHEHE");
+                   
+                    while(flag==0){
+                       bytes_received=recv(connected,recv_data,1002,0);
+                       //printf("%d ",bytes_received);
+                       int i;
+                       //if(sz<=10000)
+                           for (i=0;i<bytes_received-1 && flag==0;i++)             // Check for end marker
+                               if(recv_data[i]=='@' && recv_data[i+1]=='$'){
+                                   flag=1;
+                                   bytes_received-=2;
+                               }
+                       fwrite(recv_data,SIZE,bytes_received,fil);
+                    }
+                    fclose(fil);
+                    send(connected,"File Received",13,0);                   // Send file received message
+                }
+                if(recv_data[0]=='N' && recv_data[1]=='L' && recv_data[2]=='S' && recv_data[3]=='T'){
+                   //printf("NLST\n");
+                   fflush(stdout);
+                   send(connected,"OK",2,0);
+                   //printf("HEHEHE\n");
+                   fflush(stdout);
+                   char buff[1024];
+                   getcwd(buff,1024);
+                   printf("%s",buff);
+                   DIR *dirp = opendir(buff);
+                   struct dirent *d;
+                   while((d=readdir(dirp))!=NULL){
+                       if(strcmp(d->d_name,".")==0 || strcmp(d->d_name,"..")==0);
+                       else{
+                       send(connected,d->d_name,strlen(d->d_name)+1,0);
+                       bytes_received=recv(connected,recv_data,1024,0);
+                       }
+                       //printf("recv_data\n");
+                   }
+                   closedir(dirp);
+                   send(connected,"@$",3,0);
+                }
+                fflush(stdout);
+                
+            
+           }
+           
+           
+        /*printf("\n SEND A WORD(q to quit): ");
+        scanf("%s", send_data);
+        send(connected, send_data, strlen(send_data), 0);
+        if (strcmp(send_data, "q") == 0 || strcmp(send_data, "Q") == 0) {
+            break;
+
+        }*/
+        
+        close(connected);
+    }
+
+    close(sock);
+    return 0;
+}
